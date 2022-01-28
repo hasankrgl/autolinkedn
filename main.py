f7c485511d99c84ea400c03119e5d43b7c0e051a
Hasan Karagol <93928802+hasankrgl@users.noreply.github.com>
2022-01-28 14:13:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-chrome_path=r"C:\Users\Hasan22\Desktop\Yeni klasör\chromedriver.exe"
+chrome_path=r"Chrome Path in your Computer"
 driver=webdriver.Chrome(chrome_path)
 
-driver.get("https://www.linkedin.com/jobs/search/?f_AL=true&geoId=91000000&keywords=python%20developer&location=Avrupa%20Birli%C4%9Fi&sortBy=R")
+driver.get("URL OF THE JOBS YOU ARE APPLYING AT LINKEDLN")
 
 sign_in_button = driver.find_element_by_link_text("Oturum aç")
 sign_in_button.click()
 time.sleep(3)
 
 e_mail=driver.find_element_by_id("username")
-e_mail.send_keys("hasankaragol98@gmail.com")
+e_mail.send_keys("your e mail")
 password=driver.find_element_by_id("password")
-password.send_keys("Departed22.")
+password.send_keys("your password")
 password.send_keys(Keys.ENTER)
 
 first_result = driver.find_element_by_css_selector('.jobs-search-results ul li')
